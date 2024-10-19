9be5b7d7a7c7c7d158b55f9642ee4bb244d1afcb
nani <ronankibharath1998@gmail.com>
2024-10-18 23:47:59 -0400

    Added lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import Browse from './components/Browse'
-import Profile from './components/Profile'
-import JobDescription from './components/JobDescription'
-import Companies from './components/admin/Companies'
-import CompanyCreate from './components/admin/CompanyCreate'
-import CompanySetup from './components/admin/CompanySetup'
-import AdminJobs from "./components/admin/AdminJobs";
-import PostJob from './components/admin/PostJob'
-import Applicants from './components/admin/Applicants'
-import ProtectedRoute from './components/admin/ProtectedRoute'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import ProtectedRoute from './components/admin/ProtectedRoute';
+
+// Dynamic Imports
+const Home = lazy(() => import('./components/Home'));
+const Login = lazy(() => import('./components/auth/Login'));
+const Signup = lazy(() => import('./components/auth/Signup'));
+const Jobs = lazy(() => import('./components/Jobs'));
+const Browse = lazy(() => import('./components/Browse'));
+const Profile = lazy(() => import('./components/Profile'));
+const JobDescription = lazy(() => import('./components/JobDescription'));
+const Companies = lazy(() => import('./components/admin/Companies'));
+const CompanyCreate = lazy(() => import('./components/admin/CompanyCreate'));
+const CompanySetup = lazy(() => import('./components/admin/CompanySetup'));
+const AdminJobs = lazy(() => import('./components/admin/AdminJobs'));
+const PostJob = lazy(() => import('./components/admin/PostJob'));
+const Applicants = lazy(() => import('./components/admin/Applicants'));
 
 
 const appRouter = createBrowserRouter([
@@ -74,9 +77,9 @@ const appRouter = createBrowserRouter([
 function App() {
 
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <RouterProvider router={appRouter} />
-    </div>
+    </Suspense>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   build: {
+    chunkSizeWarningLimit: 1000, // in kB
     rollupOptions: {
       external: ['express', 'fs', 'path', 'url', 'events', 'http']
     }
